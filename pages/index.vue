dc3c1fff91eee66505bd64a7123393f60db17e80
Julia Petersson <113100420+JuliaGloggPete@users.noreply.github.com>
2024-03-11 11:39:40 +0100

    copy of old code

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,53 @@
 <template>
   <div>
-    <h2>Index</h2>
-
+    <h2>Home</h2>
   </div>
 </template>
 
-<script setup>
+<script >
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { defineComponent, reactive } from "vue";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDR9MIgBIGhGz3_WWg2kXO-0ezn2-fd37U",
+  authDomain: "skogbrynets-verkstad.firebaseapp.com",
+  projectId: "skogbrynets-verkstad",
+  storageBucket: "skogbrynets-verkstad.appspot.com",
+  messagingSenderId: "887634919102",
+  appId: "1:887634919102:web:09689068c5980d488728ef",
+  measurementId: "G-ZVPCD01LNG",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default defineComponent({
+  async setup() {
+    const productList = reactive({ list: [] });
+    const db = getFirestore(app);
+    await getProducts(db);
+    async function getProducts(db) {
+      const productsCol = collection(db, "Product");
+      const productSnapshot = await getDocs(productsCol);
+      const products = productSnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+
+      productList.list = products;
+      console.log(productList);
+    }
+
+    return {
+      productList,
+    };
+  },
+});
 </script>
 
-<style scoped>
-
-</style>
+<style></style>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,11 +1,50 @@
 <template>
   <div>
-    <h2>Products</h2>
+    <h2>Produkter</h2>
 
+    <div class="grid grid-cols-4 gap-4">
+      <div v-for="product in productList.list" :key="product.id">
+        <ProductCard :product="product" />
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup>
+<script>
+import { defineComponent, reactive, inject } from "vue";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+
+
+
+export default defineComponent({
+  async setup() {
+    const productList = reactive({ list: [] });
+    const firestore = inject("firestore");
+    const db = getFirestore(firestore);
+
+    onMounted(async () => {
+      await getProducts(db);
+    });
+
+    await getProducts(db);
+    async function getProducts(db) {
+      const productsCol = collection(db, "Product");
+      const productSnapshot = await getDocs(productsCol);
+      const products = productSnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+
+      productList.list = products;
+    }
+
+    //useHead({ title: "Skogsbrynets produkter" });
+
+    return {
+      productList,
+    };
+  },
+});
 
 </script>
 
